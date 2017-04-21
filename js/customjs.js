5b7fbf562ab0ecd80be6163c340a5f95c143f410
Valerie Fuchs <hi@valerie-fuchs.de>
2017-04-21 12:42:24 +0200

    Randomized the button to get different headers

--- js/customjs.js
+++ js/customjs.js
@@ -1,37 +1,60 @@
-  //Initializing Parralax
-  $(document).ready(function(){
-      $('.parallax').parallax();
-    });
-        
- //Reload Page when clicking on logo
+//Default hide project tiles
+$(document).ready(function () {
+
+$("#card1").hide();
+$("#card2").hide();
+$("#card3").hide();
+$("#card4").hide();
+$("#card5").hide();
+$("#card6").hide();
+$("#card7").hide();
+$("#card8").hide();
+$("#card9").hide();
+}); /*closing: document ready function*/
+
+ //Reload page when clicking on logo
  $(document).ready(function (){
  $("#headertext").on("click", function (){
  	location.reload();
   });
- });
+ });/*closing: document ready function*/
+
+//--THIS IS NEW
+//LOOPS FOR SWAPPING HEADER COPY AND BACKGROUND IMAGE
+
+//Change header copy on click
+  $(document).ready(function () {
+
+  	var headercopy = ["Bullshit Bingo","Bitterballenland","Hagelslag"];
+
+	$("#header1").on("click", function (){
+		var randomcopy = headercopy[Math.floor(Math.random()*headercopy.length)];
+		$("#headertext").text(randomcopy);
+		clearInterval(randomcopy);
+        });//end of on click
+
+	});//*closing: document ready function*/
+
+//END OF NEW STUFF
 
   //Change background on click
+
+  var backgroundimage = ["../img/beach.png","../img/palms.png","../img/beach2.png"];
+
 $(document).ready(function () {
-	$("#header2").on("click", function (){
-	$("body").removeClass("background-style");
+  $("#header2").on("click", function (){
+  	var randomimg = backgroundimage[Math.floor(Math.random()*backgroundimage.length)];
+
+    /*$("body").removeClass("background-style");
     $("body").addClass("houses");
 
     $("body").removeClass("background-style");
-    $("body").addClass("palms");
+    $("body").addClass("palms");*/
 	});
-});
-
-//Change header copy on click
-$(document).ready(function () {
-	$("#header1").on("click", function (){
-	$("#headertext").text("Bullshit Bingo");
-	});
-  });
+});/*closing: document ready function*/
 
 /*Hiding all content tiles & showing carousel*/
 $(document).ready(function () {
-
-	$("#all-cards").hide();
 
 	$("#header3").on("click", function (){
 		$("#all-cards").hide();
@@ -45,20 +68,6 @@ $(document).ready(function () {
       });/*$(document).ready(function ()*/
 	});/*$(document).ready(function ()*/
 
-//Hide project tiles default
-$(document).ready(function () {
-
-$("#card1").hide();
-$("#card2").hide();
-$("#card3").hide();
-$("#card4").hide();
-$("#card5").hide();
-$("#card6").hide();
-$("#card7").hide();
-$("#card8").hide();
-$("#card9").hide();
-
-}); /*closing: document ready function*/
 
 //Show blue cards after clicking blue button
 $(document).ready(function () {
